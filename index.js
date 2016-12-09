970aa567e8ff5f41e6df04fb3dba2bdf207dc881
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-'use strict';
+'use strict'
 
-var hljs = require('highlight.js');
-var escape = require('escape-html');
+var hljs = require('highlight.js')
+var escape = require('escape-html')
 
-exports.name = 'highlight.js';
-exports.inputFormats = ['code', 'highlight', 'highlightjs', 'highlight.js'];
-exports.outputFormat = 'html';
+exports.name = 'highlight.js'
+exports.inputFormats = ['code', 'highlight', 'highlightjs', 'highlight.js']
+exports.outputFormat = 'html'
 
 exports.render = function (str, options) {
-  options = options || {};
+  options = options || {}
   if (options.lang) {
     try {
-      return hljs.highlight(options.lang, str).value;
-    } catch (ex) {}
+      return hljs.highlight(options.lang, str).value
+    } catch (err) {}
   }
   if (options.auto || (options.auto !== false && !options.lang)) {
     try {
-      return hljs.highlightAuto(str).value;
-    } catch (ex) {}
+      return hljs.highlightAuto(str).value
+    } catch (err) {}
   }
-  return escape(str);
-};
+  return escape(str)
+}
